6a23ec8ef418d4f2fd0283d2ccbf6c4ad01788b4
rekanyeste <rekabeka37@gmail.com>
2026-05-26 21:16:26 +0200

    fix: persistence is enabled from now for the FireStore and the size of the cache has increased for better test output

--- Marketplace/lib/main.dart
+++ Marketplace/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,6 +25,11 @@ void main() async {
   );
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+    cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+  );
 
   // Background handler only on mobile – not supported on web
   if (!kIsWeb) {
